960f3c7d11f9b52bb39c06fe589ceaa26d066c73
Juan Cruz Rey Alvarez <juancurey2@gmail.com>
2021-04-22 17:38:26 -0300

    Ejercicio Terminado Clase3

--- src/Library.hs
+++ src/Library.hs
@@ -1,5 +1,36 @@
 module Library where
 import PdePreludat
 
-doble :: Number -> Number
-doble numero = numero + numero
+type Horas = Number
+type Presion = Number
+type Fortaleza = String
+
+presionGolpe :: Horas -> Fortaleza -> Presion
+presionGolpe horas fortaleza = poder horas/ objetivo fortaleza
+
+objetivo :: Fortaleza-> Number
+objetivo = (*2) . length
+
+poder :: Horas -> Number
+poder = (*15)
+
+
+
+type Golpe = Fortaleza -> Number
+
+gomuGomu :: Golpe
+gomuGomu = presionGolpe 180
+
+normalesConsecutivos :: Golpe
+normalesConsecutivos = presionGolpe 240
+
+type Caractobjetivo = Fortaleza -> Bool
+ 
+dificil :: Caractobjetivo
+dificil fortaleza = ( (gomuGomu fortaleza) < 100) 
+
+focalizarObjetivo :: Fortaleza -> Fortaleza
+focalizarObjetivo fortaleza = take 7 fortaleza
+
+accesible :: Caractobjetivo
+accesible fortaleza =  ( normalesConsecutivos ( focalizarObjetivo fortaleza ) > 200) && ( normalesConsecutivos ( focalizarObjetivo fortaleza ) < 400)
